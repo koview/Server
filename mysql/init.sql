bf7ea5d93b07a79c7d6184d046e583a71bb64e43
Jihyun3478 <leejihyun3478@gmail.com>
2024-07-09 16:48:02 +0900

    feat: member 더미데이터 추가

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,1 +1,20 @@
 CREATE DATABASE IF NOT EXISTS kodb;
+USE kodb;
+
+CREATE TABLE IF NOT EXISTS Member (
+    MEMBER_ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    loginPw VARCHAR(255) NOT NULL,
+    verifiedPw VARCHAR(255) NOT NULL,
+    nickname VARCHAR(255) NOT NULL UNIQUE,
+    age INT,
+    role VARCHAR(30) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+INSERT INTO Member (email, loginPw, verifiedPw, nickname, age, role)
+VALUES ('test1@example.com', 'password1', 'password1', 'User1', 20, 'USER');
+
+INSERT INTO Member (email, loginPw, verifiedPw, nickname, age, role)
+VALUES ('admin@example.com', 'adminpw', 'adminpw', '관리자', 20, 'ADMIN');

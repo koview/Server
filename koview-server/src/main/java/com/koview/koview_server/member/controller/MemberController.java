84dd9394a6e2c7fda030390c117470ca9dc2e529
Jihyun3478 <leejihyun3478@gmail.com>
2024-07-09 16:21:51 +0900

    feat: Swagger 구현

--- koview-server/src/main/java/com/koview/koview_server/member/controller/MemberController.java
+++ koview-server/src/main/java/com/koview/koview_server/member/controller/MemberController.java
@@ -9,18 +9,21 @@ import com.koview.koview_server.member.domain.dto.LoginRequestDTO;
 import com.koview.koview_server.member.domain.dto.SignupRequestDTO;
 import com.koview.koview_server.member.domain.dto.SignupResponseDTO;
 import com.koview.koview_server.member.service.MemberServiceImpl;
-import jakarta.annotation.PostConstruct;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
+@Tag(name = "Member", description = "Member API")
 public class MemberController {
 
     private final MemberServiceImpl memberServiceImpl;
 
     @GetMapping("/confirmEmail/{email}")
+    @Operation(description = "회원가입 시 이메일 중복 확인")
     public ApiResult<?> confirmEmail(@PathVariable("email") String email) {
         if (memberServiceImpl.confirmEmail(email)) {
             throw new MemberException(ErrorStatus.MEMBER_EMAIL_ALREADY_EXISTS);
@@ -29,6 +32,7 @@ public class MemberController {
     }
 
     @GetMapping("/confirmName/{name}")
+    @Operation(description = "회원가입 시 닉네임 중복 확인")
     public ApiResult<?> confirmName(@PathVariable("name") String name) {
         if (memberServiceImpl.confirmName(name)) {
             throw new MemberException(ErrorStatus.MEMBER_NAME_ALREADY_EXISTS);
@@ -37,33 +41,23 @@ public class MemberController {
     }
 
     @PostMapping("/signup")
-    public ApiResult<?> signup(@RequestBody SignupRequestDTO requestDTO) {
+    @Operation(description = "회원가입")
+    public ApiResult<SignupResponseDTO> signup(@RequestBody SignupRequestDTO requestDTO) {
         SignupResponseDTO responseDTO = memberServiceImpl.createMember(requestDTO);
         return ApiResult.onSuccess(responseDTO);
     }
 
     @PostMapping("/signin")
-    public ApiResult<?> signIn(@RequestBody LoginRequestDTO requestDTO) {
+    @Operation(description = "로그인")
+    public ApiResult<JwtTokenDTO> signIn(@RequestBody LoginRequestDTO requestDTO) {
         JwtTokenDTO jwtTokenDTO = memberServiceImpl.signIn(requestDTO);
 
         return ApiResult.onSuccess(jwtTokenDTO);
     }
 
     @PostMapping("/reissue")
-    public ApiResult<?> reissue(@RequestBody ReissueRequestDTO requestDTO) {
+    @Operation(description = "Access 토큰 만료 시 재발급")
+    public ApiResult<JwtTokenDTO> reissue(@RequestBody ReissueRequestDTO requestDTO) {
         return ApiResult.onSuccess(memberServiceImpl.reissue(requestDTO));
-    }
-
-//    @PostConstruct
-    public void initMemberData() {
-        SignupRequestDTO member1 = SignupRequestDTO.builder()
-                .email("test@gmail.com")
-                .loginPw("test1234")
-                .verifiedPw("test1234")
-                .name("이름")
-                .age(20)
-                .build();
-
-        memberServiceImpl.createMember(member1);
     }
 }

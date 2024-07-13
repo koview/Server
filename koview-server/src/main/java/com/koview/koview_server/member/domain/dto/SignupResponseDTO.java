eadc343de9dd461a2073dba658f57cef0f9e8149
Jihyun3478 <leejihyun3478@gmail.com>
2024-07-13 22:39:48 +0900

    fix: SignupResponseDTO에 패스워드 관련 필드 삭제

--- koview-server/src/main/java/com/koview/koview_server/member/domain/dto/SignupResponseDTO.java
+++ koview-server/src/main/java/com/koview/koview_server/member/domain/dto/SignupResponseDTO.java
@@ -11,17 +11,13 @@ import lombok.Getter;
 public class SignupResponseDTO {
     private final Long id;
     private final String email;
-    private final String loginPw;
-    private final String originLoginPw;
     private final String nickname;
     private final int age;
 
     /* Entity -> DTO */
-    public SignupResponseDTO(Member member, String originLoginPw) {
+    public SignupResponseDTO(Member member) {
         this.id = member.getId();
         this.email = member.getEmail();
-        this.loginPw = member.getLoginPw();
-        this.originLoginPw = originLoginPw;
         this.nickname = member.getNickname();
         this.age = member.getAge();
     }

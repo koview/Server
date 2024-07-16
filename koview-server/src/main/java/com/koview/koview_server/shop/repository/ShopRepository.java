919a2c2057b865e82a4225691ace299b761bbe64
Jihyun3478 <leejihyun3478@gmail.com>
2024-07-16 18:34:23 +0900

    feat: Shop 전체 조회 API 구현

--- koview-server/src/main/java/com/koview/koview_server/shop/repository/ShopRepository.java
+++ koview-server/src/main/java/com/koview/koview_server/shop/repository/ShopRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ShopRepository extends JpaRepository<Shop, Long> {
-    List<Shop> findAllByShopNameIn(List<String> shopName);
+    List<Shop> findAllByIdIn(List<Long> shopId);
 }

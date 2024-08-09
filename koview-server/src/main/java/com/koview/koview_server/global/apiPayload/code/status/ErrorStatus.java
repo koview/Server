95f4cc7da9fbaae22b4617165957f276db70f0a5
Jihyun3478 <leejihyun3478@gmail.com>
2024-08-09 15:59:45 +0900

    fix: ErrorStatus 수정

--- koview-server/src/main/java/com/koview/koview_server/global/apiPayload/code/status/ErrorStatus.java
+++ koview-server/src/main/java/com/koview/koview_server/global/apiPayload/code/status/ErrorStatus.java
@@ -30,8 +30,8 @@ public enum ErrorStatus implements BaseErrorCode {
     MEMBER_PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "MEMBER4004", "비밀번호가 일치하지 않습니다."),
     NOT_MATCHING_MEMBER_TOKEN(HttpStatus.BAD_REQUEST, "MEMBER4005", "토큰과 로그인 된 멤버가 일치하지 않습니다."),
     MEMBER_ACCESS_TOKEN_IS_EXPIRED(HttpStatus.BAD_REQUEST, "MEMBER4006", "만료된 Access Token 입니다. 재발급을 요청하세요."),
-    MEMBER_REFRESH_TOKEN_IS_EXPIRED(HttpStatus.BAD_REQUEST, "MEMBER4007", "만료된 Access Token 입니다. 재발급을 요청하세요."),
-    MEMBER_WAS_SIGN_OUT(HttpStatus.BAD_REQUEST, "MEMBER4007", "로그아웃 된 사용자입니다."),
+    MEMBER_REFRESH_TOKEN_IS_EXPIRED(HttpStatus.BAD_REQUEST, "MEMBER4007", "만료된 Refresh Token 입니다. 재발급을 요청하세요."),
+    MEMBER_WAS_SIGN_OUT(HttpStatus.BAD_REQUEST, "MEMBER4008", "로그아웃 된 사용자입니다."),
 
     // 리뷰 응답
     REVIEW_NOT_FOUND(HttpStatus.BAD_REQUEST, "REVIEW4001", "존재하지 않는 리뷰입니다."),

d93426f5d508ab253fb149e0ca64ebcfccd0fb9f
Jihyun3478 <leejihyun3478@gmail.com>
2024-08-12 10:13:30 +0900

    fix: 로그인한 유저의 좋아요 여부 함께 반환

--- koview-server/src/main/java/com/koview/koview_server/member/domain/Member.java
+++ koview-server/src/main/java/com/koview/koview_server/member/domain/Member.java
@@ -59,6 +59,8 @@ public class Member extends BaseTimeEntity {
     @Column(nullable = false, length = 30)
     private RoleType role; // 권한
 
+    private Boolean isCurrentMemberLiked = false;
+
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Review> reviewList = new ArrayList<>();
 
@@ -103,5 +105,13 @@ public class Member extends BaseTimeEntity {
 
     public void addProfileImage(ProfileImage profileImage) {
         this.profileImage = profileImage;
+    }
+
+    public void isCurrentMemberLikedTrue() {
+        this.isCurrentMemberLiked = true;
+    }
+
+    public void isCurrentMemberLikedFalse() {
+        this.isCurrentMemberLiked = false;
     }
 }

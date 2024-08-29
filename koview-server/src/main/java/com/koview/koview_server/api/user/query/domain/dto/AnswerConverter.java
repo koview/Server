7935d9a5967d1dd5929927965209a897229eb1df
loading1031 <tjdans1031@gmail.com>
2024-08-29 16:39:12 +0900

    hotfix: 답변 response 도 profileResponseDTO 및 review_id 추가

--- koview-server/src/main/java/com/koview/koview_server/api/user/query/domain/dto/AnswerConverter.java
+++ koview-server/src/main/java/com/koview/koview_server/api/user/query/domain/dto/AnswerConverter.java
@@ -1,5 +1,8 @@
 package com.koview.koview_server.api.user.query.domain.dto;
 
+import com.koview.koview_server.api.auth.member.domain.Member;
+import com.koview.koview_server.api.common.ProfileResponseDTO;
+import com.koview.koview_server.api.image.domain.ProfileImage;
 import com.koview.koview_server.api.user.query.domain.QueryAnswer;
 import com.koview.koview_server.api.image.domain.dto.ImageResponseDTO;
 import com.koview.koview_server.api.user.review.domain.Review;
@@ -13,16 +16,19 @@ public class AnswerConverter {
         List<ImageResponseDTO> imageList = review.getReviewImageList().
                 stream().map(ImageResponseDTO::new).limit(2).toList();
 
+        ProfileResponseDTO profileInfo = getProfileInfo(review.getMember());
+
         Long likeCount = review.getTotalLikesCount();
 
         return AnswerResponseDTO.Single.builder()
                 .answerId(queryAnswer.getId())
+                .profileInform(profileInfo)
                 .imageList(imageList)
                 .content(queryAnswer.getContent())
-                .writer(queryAnswer.getMember().getNickname())
                 .commentCount((long) review.getCommentList().size())
                 .totalLikeCount(likeCount != null ? likeCount : 0)
                 .isLiked(isLiked)
+                .reviewId(review.getId())
                 .updatedAt(queryAnswer.getLastModifiedDate().toLocalDate())
                 .createdAt(queryAnswer.getCreatedDate().toLocalDate())
                 .build();
@@ -39,6 +45,17 @@ public class AnswerConverter {
                 .getTotalElements(queryAnswerPage.getTotalElements())
                 .isFirst(queryAnswerPage.isFirst())
                 .isLast(queryAnswerPage.isLast())
+                .build();
+    }
+    private static ProfileResponseDTO getProfileInfo(Member member){
+        ProfileImage profileImage = member.getProfileImage();
+        boolean isProfileImage = profileImage != null;
+
+        return ProfileResponseDTO.builder()
+                .imageId(isProfileImage ? profileImage.getId() : null)
+                .imageUrl(isProfileImage ? profileImage.getUrl() : null)
+                .memberId(member.getId())
+                .memberNickname(member.getNickname())
                 .build();
     }
 }
